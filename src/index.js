b8de23c72faa46ccd982ccbb9c3d9f343c0db4b7
Ashah15 <ashaabdillahi53@gmail.com>
2020-10-02 01:00:12 +0300

    Add priority in the todo.js file

--- src/index.js
+++ src/index.js
@@ -4,24 +4,6 @@ import './styles.css';
 
 let importedProject = Projects();
 
-class TodoItem {
-  constructor(title,
-    description,
-    dueDate = 'default',
-    priority = '5',
-    notes = '',
-    checklist = '',
-    project = projects[0]) {
-    this.title = title;
-    this.description = description;
-    this.dueDate = dueDate;
-    this.priority = priority;
-    this.notes = notes;
-    this.checklist = checklist;
-    this.project = project;
-  }
-}
-
 window.onload = () => {
   let projects = importedProject.getList();
   projectOptions(projects);
--- src/todo.js
+++ src/todo.js
@@ -23,13 +23,48 @@ const toDoPage = () => {
     toDoProjectTitle.innerHTML = name;
 
     const addToDoButton = document.createElement('button');
-    addToDoButton.innerHTML = 'Add Task';
+    addToDoButton.innerHTML = 'Add New Task';
     addToDoButton.setAttribute('class', 'btn btn-primary todo-button');
     
     toDoDiv.appendChild(toDoProjectTitle);
     toDoDiv.appendChild(addToDoButton);
 
-    const 
+    const toDoSectionMainDiv = document.createElement('div');
+    toDoSectionMainDiv.setAttribute('class', 'todo-section-div ');
+
+    toDoDiv.appendChild(toDoSectionMainDiv);
+
+    for (let i = 0; i < toDoList.length; i += 1) {
+      if (value === toDoList[i].project) {
+        const todoSection = document.createElement('div');
+        todoSection.setAttribute('class', 'todo-section');
+        const toDoParagraph = document.createElement('p');
+        toDoParagraph.setAttribute('class', 'p-title');
+        toDoParagraph.innerHTML = toDoList[i].title;
+        const toDoDate = document.createElement('p');
+        toDoDate.setAttribute('class', 'p-date');
+        toDoDate.innerHTML = toDoList[i].duedate;
+
+        todoSection.appendChild(toDoParagraph);
+        todoSection.appendChild(toDoDate);
+
+        if (toDoList[i].priority === 'High') {
+          const toDoSectionPrority = document.createElement('p');
+          toDoSectionPrority.setAttribute('class', 'high-priority');
+          toDoSectionPrority.innerHTML = toDoList[i].priority;
+          todoSection.appendChild(toDoSectionPrority);
+        } else if (toDoList[i].priority === 'Medium') {
+          const toDoSectionPrority = document.createElement('p');
+          toDoSectionPrority.setAttribute('class', 'medium-priority');
+          toDoSectionPrority.innerHTML = toDoList[i].priority;
+          todoSection.appendChild(toDoSectionPrority);
+        } else if (toDoList[i].priority === 'Low') {
+          const toDoSectionPrority = document.createElement('p');
+          toDoSectionPrority.setAttribute('class', 'low-priority');
+          toDoSectionPrority.innerHTML = toDoList[i].priority;
+          todoSection.appendChild(toDoSectionPrority);
+        }
+
 
 
 
